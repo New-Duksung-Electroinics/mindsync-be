e4aa89553b827e9c7cf845143aa228796e30b016
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-01-30 14:24:32 +0900

    feat : 로그인 기능 구현
    jwt 토큰 발급 전 스프링 시큐리티 이메일 비번 검증 필터 구현했습니다.

--- src/main/java/com/mindsync/mindsync/config/SecurityConfig.java
+++ src/main/java/com/mindsync/mindsync/config/SecurityConfig.java
@@ -1,18 +1,32 @@
 package com.mindsync.mindsync.config;
 
 
+import com.mindsync.mindsync.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
     // 비밀번호 암호화
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -38,6 +52,8 @@ public class SecurityConfig {
                         .permitAll()
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .anyRequest().authenticated());
+
+        http    .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         http
                 .sessionManagement((session) -> session
