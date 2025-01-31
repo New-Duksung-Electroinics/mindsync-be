f7dc7c1ad722cb3c21a3e5ec48e8d550a36915bb
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-01-31 14:48:26 +0900

    feat : cors 설정
    임시로 모든 port 허용

---
+++ src/main/java/com/mindsync/mindsync/config/CorsMvcConfig.java
@@ -0,0 +1,15 @@
+package com.mindsync.mindsync.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*"); // 일단 모든 포트 허용으로 열어둠
+    }
+}
