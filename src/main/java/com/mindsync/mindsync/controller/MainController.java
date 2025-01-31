e9a50fa6fa0dac4da8603d5c45b536df6a61f322
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-01-31 14:20:49 +0900

    feat : 로그인 후 세션 정보 확인 추가

--- src/main/java/com/mindsync/mindsync/controller/MainController.java
+++ src/main/java/com/mindsync/mindsync/controller/MainController.java
@@ -1,9 +1,15 @@
 package com.mindsync.mindsync.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @RestController
 public class MainController {
@@ -11,6 +17,14 @@ public class MainController {
     // 테스트 API
     @GetMapping("/")
     public String mainP() {
-        return "main Controller";
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "main Controller" + name + role;
     }
 }
