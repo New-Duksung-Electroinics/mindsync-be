3eebf371d3a1835f7d11a6c482f1a1b2e405dc97
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-02-03 22:43:32 +0900

    add : application.properties 임시 업로드

--- src/main/java/com/mindsync/mindsync/controller/MainController.java
+++ src/main/java/com/mindsync/mindsync/controller/MainController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Collection;
 import java.util.Iterator;
 
