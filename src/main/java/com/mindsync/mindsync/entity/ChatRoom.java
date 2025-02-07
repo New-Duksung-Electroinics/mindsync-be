f473cd3e56117bca025ace65e939a857c24c0067
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-02-07 23:39:59 +0900

    feat : chatroom 엔터티 구현

---
+++ src/main/java/com/mindsync/mindsync/entity/ChatRoom.java
@@ -0,0 +1,40 @@
+package com.mindsync.mindsync.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "chatroom")
+public class ChatRoom {
+
+    @Id
+    private String id;
+
+    private String roomId;
+    private String title; // 회의 제목
+    private String host; // 회의 주최자 이름
+    private List<String> participants; // 참여 멤버 (이메일 리스트)
+    private String content; // 회의 예상 내용
+    private List<String> agenda; // AI가 생성한 회의 목차
+
+    public static ChatRoom create(String title, String host, List<String> participants, String content, List<String> agenda) {
+        return ChatRoom.builder()
+                .id(UUID.randomUUID().toString())
+                .roomId(UUID.randomUUID().toString()) // 채팅방의 고유 ID
+                .title(title)
+                .host(host)
+                .participants(participants)
+                .content(content)
+                .agenda(agenda)
+                .build();
+    }
+
+}
