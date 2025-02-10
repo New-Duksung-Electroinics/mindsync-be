63999f977f987b9297e2c200e70f5601a33a55db
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-02-10 23:07:41 +0900

    fix : cors 수정

--- src/main/java/com/mindsync/mindsync/config/CorsMvcConfig.java
+++ src/main/java/com/mindsync/mindsync/config/CorsMvcConfig.java
@@ -4,16 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsMvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // 일단 모든 포트 허용으로 열어둠
-                .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
-                .allowedHeaders("*")  // 허용할 헤더
-                .allowCredentials(true);  // 쿠키를 포함한 요청을 허용
-//                .exposedHeaders("access", "Set-Cookie");
-    }
-}
+//@Configuration
+//public class CorsMvcConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry corsRegistry) {
+//        corsRegistry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000") // 일단 모든 포트 허용으로 열어둠
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
+//                .allowedHeaders("*")  // 허용할 헤더
+//                .allowCredentials(true);  // 쿠키를 포함한 요청을 허용
+////                .exposedHeaders("access", "Set-Cookie");
+//    }
+//}
