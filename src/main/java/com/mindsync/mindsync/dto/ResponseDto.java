9a686fb643ae0a3f86e31d97479333e5d1115dd8
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-02-11 22:41:57 +0900

    refactor : 불필요한 import 및 코드 삭제

--- src/main/java/com/mindsync/mindsync/dto/ResponseDto.java
+++ src/main/java/com/mindsync/mindsync/dto/ResponseDto.java
@@ -3,7 +3,6 @@ package com.mindsync.mindsync.dto;
 import com.mindsync.mindsync.utils.ResponseStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
