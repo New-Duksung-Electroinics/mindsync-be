5408df564da09727a7925ea8d23ed13b5f934ba5
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-02-13 11:31:40 +0900

    feat : 채팅방 초대를 위한 이메일 검색 기능 완성

--- src/main/java/com/mindsync/mindsync/controller/ChatRoomController.java
+++ src/main/java/com/mindsync/mindsync/controller/ChatRoomController.java
@@ -1,22 +1,29 @@
 package com.mindsync.mindsync.controller;
 
 import com.mindsync.mindsync.dto.ChatRoomRequestDTO;
+import com.mindsync.mindsync.dto.EmailSearchDto;
 import com.mindsync.mindsync.dto.ResponseDto;
 import com.mindsync.mindsync.entity.ChatRoom;
 import com.mindsync.mindsync.service.ChatRoomService;
+import com.mindsync.mindsync.service.UserService;
 import com.mindsync.mindsync.utils.ResponseUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/chat")
 public class ChatRoomController {
     private final ChatRoomService chatRoomService;
+    private final UserService userService;
 
-    public ChatRoomController(ChatRoomService chatRoomService) {
+    public ChatRoomController(ChatRoomService chatRoomService, UserService userService) {
         this.chatRoomService = chatRoomService;
+        this.userService = userService;
     }
 
     @PostMapping("/room")
@@ -35,6 +42,17 @@ public class ChatRoomController {
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body(ResponseUtil.ERROR("서버 에러가 발생했습니다.", null)).getBody();
         }
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<ResponseDto<List<EmailSearchDto>>> searchUsers(@RequestParam String query) {
+        List<EmailSearchDto> users = userService.searchUsersByEmail(query);
+
+        if (users.isEmpty()) {
+            return ResponseEntity.ok(ResponseUtil.SUCCESS("검색 결과가 없습니다.", Collections.emptyList()));
+        }
+
+        return ResponseEntity.ok(ResponseUtil.SUCCESS("검색 완료했습니다.", users));
     }
 }
 
