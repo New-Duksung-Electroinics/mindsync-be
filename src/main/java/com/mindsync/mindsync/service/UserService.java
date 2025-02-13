be70a2efc869a3b3fd406f08accdb0c818372920
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-02-13 11:07:06 +0900

    feat : query로 시작하는 모든 이메일 찾아 리스트로 전달하도록 구현

--- src/main/java/com/mindsync/mindsync/service/UserService.java
+++ src/main/java/com/mindsync/mindsync/service/UserService.java
@@ -1,8 +1,12 @@
 package com.mindsync.mindsync.service;
 
+import com.mindsync.mindsync.dto.EmailSearchDto;
 import com.mindsync.mindsync.entity.User;
 import com.mindsync.mindsync.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -12,5 +16,11 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public ResponseDto
+    public List<EmailSearchDto> searchUsersByEmail(String query) {
+        List<User> users = userRepository.findByEmailStartingWith(query);
+
+        return users.stream()
+                .map(user -> new EmailSearchDto(user.getEmail()))
+                .collect(Collectors.toList());
+    }
 }
