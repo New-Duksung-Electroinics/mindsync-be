7e99f340a6e6d36128e204b421012da8db26cd69
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-02-15 15:30:07 +0900

    feat : 웹소켓, stomp 설정

---
+++ src/main/java/com/mindsync/mindsync/config/WebSocketConfig.java
@@ -0,0 +1,33 @@
+package com.mindsync.mindsync.config;
+
+import com.mindsync.mindsync.jwt.JWTUtil;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JWTUtil jwtUtil;
+
+    public WebSocketConfig(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-chat")
+                .setAllowedOriginPatterns("*")
+                .addInterceptors()
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
