ee430aace128409867fd6ff9971ca82159d3a561
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-02-18 19:06:16 +0900

    feat : 채팅방 송수신 기능 구현

--- src/main/java/com/mindsync/mindsync/controller/ChatMessageController.java
+++ src/main/java/com/mindsync/mindsync/controller/ChatMessageController.java
@@ -18,9 +18,17 @@ public class ChatMessageController {
 
     @MessageMapping("/chat/{roomId}/enter")
     public void enterChatRoom(@Payload ChatMessageDTO message, @DestinationVariable String roomId) {
-        System.out.println("✅ enter send: " + message.getSender());
+        System.out.println("enter send: " + message.getSender());
         message.setMessage(message.getSender() + "is enter the room");
         messagingTemplate.convertAndSend("/topic/chat/" + roomId, message);
-        System.out.println("✅ message success: " + message.getMessage());
+        System.out.println("message success: " + message.getMessage());
+    }
+
+    // 채팅 메시지 전송 처리
+    @MessageMapping("/chat/{roomId}/send")
+    public void sendMessage(@Payload ChatMessageDTO message, @DestinationVariable String roomId) {
+        System.out.println("Message received from " + message.getSender() + ": " + message.getMessage());
+        messagingTemplate.convertAndSend("/topic/chat/" + roomId, message);
+        System.out.println("Message sent to room: " + roomId);
     }
 }
