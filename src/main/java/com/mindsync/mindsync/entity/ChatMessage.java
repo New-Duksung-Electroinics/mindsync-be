1bec4873a7f0850b751424cd09067f9214b108b8
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-02-27 19:21:17 +0900

    feat : chatmessage 엔터티에 timestamp 추가
    채팅 보낸 시간 확인하기 위해

--- src/main/java/com/mindsync/mindsync/entity/ChatMessage.java
+++ src/main/java/com/mindsync/mindsync/entity/ChatMessage.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +18,7 @@ public class ChatMessage {
     @Id
     private String roomId;
 
+    // 목차 별로 그룹화 시키기 위해
     private Map<Integer, List<Message>> messages;
 
     @Getter
@@ -27,6 +29,7 @@ public class ChatMessage {
     public static class Message{
         private String sender;
         private String message;
+        private LocalDateTime timestamp;
     }
 
 }
