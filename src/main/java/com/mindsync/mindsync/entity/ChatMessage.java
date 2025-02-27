f959812430cd0c7955a0ff99ea568ea1487149f6
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-02-27 17:47:41 +0900

    feat : 채팅 저장위한 컬렉션 생성

---
+++ src/main/java/com/mindsync/mindsync/entity/ChatMessage.java
@@ -0,0 +1,32 @@
+package com.mindsync.mindsync.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "chat")
+public class ChatMessage {
+    @Id
+    private String roomId;
+
+    private Map<Integer, List<Message>> messages;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Message{
+        private String sender;
+        private String message;
+    }
+
+}
