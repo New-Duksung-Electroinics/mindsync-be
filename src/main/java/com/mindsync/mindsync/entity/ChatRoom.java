413c040f24943d88d00441f1649d65855be7ff32
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-03-29 15:54:34 +0900

    feat : 방 생성시 host 이름이 아닌 host 이메일로 수정 및 mbti 추가 request 하도록 수정

--- src/main/java/com/mindsync/mindsync/entity/ChatRoom.java
+++ src/main/java/com/mindsync/mindsync/entity/ChatRoom.java
@@ -17,28 +17,31 @@ public class ChatRoom {
     private String id;
     private String roomId;
     private String title; // 회의 제목
-    private String host; // 회의 주최자 이름
+    private String host_email; // 회의 주최자 이메일
     private List<String> participants; // 참여 멤버 (이메일 리스트)
     private String content; // 회의 예상 내용
+    private String mbti; // 헬퍼 mbti
 
     @Builder
-    public ChatRoom(String id, String roomId, String title, String host, List<String> participants, String content) {
+    public ChatRoom(String id, String roomId, String title, String host_email, List<String> participants, String content, String mbti) {
         this.id = id;
         this.roomId = roomId;
         this.title = title;
-        this.host = host;
+        this.host_email = host_email;
         this.participants = participants;
         this.content = content;
+        this.mbti = mbti;
     }
 
-    public static ChatRoom create(String title, String host, List<String> participants, String content) {
+    public static ChatRoom create(String title, String host_email, List<String> participants, String content, String mbti) {
         return ChatRoom.builder()
                 .id(UUID.randomUUID().toString())
                 .roomId(UUID.randomUUID().toString()) // 채팅방의 고유 ID
                 .title(title)
-                .host(host)
+                .host_email(host_email)
                 .participants(participants)
                 .content(content)
+                .mbti(mbti)
                 .build();
     }
 
