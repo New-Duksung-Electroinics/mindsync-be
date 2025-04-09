27bf920320335676fe7fb16592a27654858c292f
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-04-09 21:14:46 +0900

    feat : accessToken 시간 변경

--- src/main/java/com/mindsync/mindsync/jwt/LoginFilter.java
+++ src/main/java/com/mindsync/mindsync/jwt/LoginFilter.java
@@ -76,7 +76,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         String role = auth.getAuthority();
 
         // JWT 토큰 생성
-        String access = jwtUtil.createJwt("access", email, role, 600000L);
+        String access = jwtUtil.createJwt("access", email, role, 36000000L);
         String refresh = jwtUtil.createJwt("refresh", email, role, 86400000L);
 
         // refresh 토큰 저장
