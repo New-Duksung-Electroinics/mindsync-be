9748ffe82ed829766040d5c1840965030c1d996b
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-07-06 19:35:50 +0900

    fix : 목차 수정 API 구현 후 충돌 해결

--- src/main/java/com/mindsync/mindsync/dto/AgendaUpdateRequest.java
+++ src/main/java/com/mindsync/mindsync/dto/AgendaUpdateRequest.java
@@ -8,9 +8,6 @@ import java.util.Map;
 @Getter
 @Setter
 public class AgendaUpdateRequest {
-<<<<<<< HEAD
-    private Map<String, String> data;
-=======
+
     private Map<String, String> data; // 클라이언트 요청 형태
->>>>>>> 4da74902baf2e0054ffb028cedd1812df63a67a1
 }
