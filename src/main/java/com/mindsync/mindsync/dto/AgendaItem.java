afe55a6200e3a10f5d43c0ced19c992a696604ad
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-07-06 19:11:23 +0900

    fix : 목차 수정 API 테스트 및 수정 완료

--- src/main/java/com/mindsync/mindsync/dto/AgendaItem.java
+++ src/main/java/com/mindsync/mindsync/dto/AgendaItem.java
@@ -2,10 +2,12 @@ package com.mindsync.mindsync.dto;
 
 import lombok.*;
 
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
 @AllArgsConstructor
 public class AgendaItem {
     private String title; // 목차 내용
     private String status; // 목차 진행 상태
+
+
 }
---
+++ src/main/java/com/mindsync/mindsync/service/AgendaService.java
@@ -0,0 +1,26 @@
+package com.mindsync.mindsync.service;
+
+import com.mindsync.mindsync.dto.AgendaItem;
+import com.mindsync.mindsync.entity.Agenda;
+import com.mindsync.mindsync.repository.AgendaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AgendaService {
+    private final AgendaRepository agendaRepository;
+
+    public void updateAgenda(String roomId, Map<String, String> rawData) {
+        Map<String, AgendaItem> agendas = new HashMap<>();
+        rawData.forEach((key, title) -> {
+            agendas.put(key, new AgendaItem(title, "pending"));
+        });
+
+        Agenda agenda = new Agenda(roomId, agendas);
+        agendaRepository.save(agenda);
+    }
+}
